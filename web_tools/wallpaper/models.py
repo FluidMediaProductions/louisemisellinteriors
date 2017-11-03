98cae2a78cd735e0ef076684ee45ef80e1ff89b0
Benjamin Misell <beninjam174@gmail.com>
2017-11-03 15:06:03 +0000

    Create wallpaper model

--- web_tools/wallpaper/models.py
+++ web_tools/wallpaper/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Wallpaper(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField('Repeatable Image')
+    width = models.IntegerField('Width (cm)')
+    roll_length = models.FloatField('Roll length (m)')
+    repeat = models.IntegerField('Pattern repeat (cm)')
